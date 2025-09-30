5f1d5e2f9ec83c310010e688bdf9fa5da4dc8345
marcoscaio22-arch <marcoscaio22@gmail.com>
2025-09-30 06:04:57 +0000

    Movimento Bispo

--- Bispo.c
+++ Bispo.c
@@ -5,7 +5,7 @@ int main() {
     
     //Mover o Bispo 5 vezes na Diagonal//
 for(int i = 0; i < 5; i++){
-    printf("Diagonal\n"); //imprimir a direção do movimento
+    printf("Diagonal Direita\n"); //imprimir a direção do movimento
 }
     
 
