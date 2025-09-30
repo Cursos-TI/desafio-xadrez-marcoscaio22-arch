4137b871acc44b0b309c4789e4a1f2b6f820706c
marcoscaio22-arch <marcoscaio22@gmail.com>
2025-09-30 06:18:29 +0000

    Rainha movimento

---
+++ Rainha.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+    
+    char i = 1;
+    // Mover a Raina 8 vezes para Horizontal Esquerda
+    while (i <= 8)
+    {
+        printf("Horizontal Esquerda\n", i); // Imprimir a direção do movimento
+        i++;
+    }
+
+    return 0;
+}
