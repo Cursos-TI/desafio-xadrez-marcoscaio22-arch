db53dd02912add9b4cc5ebb107540eead5c290e1
marcoscaio22-arch <marcoscaio22@gmail.com>
2025-09-30 06:01:06 +0000

    movimento do bispo

---
+++ Bispo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main() {
+    
+    //Mover o Bispo 5 vezes na Diagonal//
+for(int i = 0; i < 5; i++){
+    printf("Diagonal\n"); //imprimir a direção do movimento
+}
+    
+
+
+    return 0;
+}
